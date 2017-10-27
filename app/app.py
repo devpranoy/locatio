c878ee82b7a3f7356460af6649c3e78b04f91c84
__SyS__ <sreyassreelal@gmail.com>
2017-10-27 12:37:34 +0530

    added login page

---
+++ app/app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask,render_template,send_from_directory
+ 
+app = Flask(__name__,static_url_path='/static' )
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+@app.route('/login')
+def chat():
+ 
+    return render_template('login.html')
+ 
+ 
+app.run(host = 'localhost',port=80,debug=True)
